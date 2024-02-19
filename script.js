2db1d8b5be56910d9c53c68beb93aebdeb2a0173
imxdh <74244566+imxdh@users.noreply.github.com>
2024-02-19 17:40:56 +0000

    Make game playable from console.

---
+++ script.js
@@ -0,0 +1,75 @@
+const gameBoard = (() => {
+  const board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+
+  const displayBoard = () => {
+    console.log(board);
+  }
+
+  let currentMarker = '';
+
+  const placeMarker = (cellNumber) => {
+    if (cellNumber < 0 || cellNumber > 8) {
+      console.log('Error - input number from 0 - 8');
+      return;
+    } 
+
+    const row = Math.floor(cellNumber / 3);
+    const col = cellNumber % 3;
+    
+    if (board[row][col] === '') {
+      if (currentMarker === '' || currentMarker === 'o') {
+        board[row][col] = 'x';
+        currentMarker = 'x'
+        displayBoard();
+        checkForWin();
+      } else if (currentMarker === 'x') {
+        board[row][col] = 'o';
+        currentMarker = 'o'
+        displayBoard();
+        checkForWin();
+      }
+    } 
+    else {
+      console.log('Choose a different number, this cell is already filled');
+    }
+  };
+
+  const checkForWin = () => {
+    winningCombos.forEach((combo) => {
+      const [a, b, c] = combo;
+
+      const marker = board[Math.floor(a / 3)][a % 3];
+
+      if (marker && marker === board[Math.floor(b / 3)][b % 3] && marker === board[Math.floor(c / 3)][c % 3]) {
+        winningComboFound = true;
+        console.log(`${currentMarker} wins!`)
+      }
+    });
+  };
+
+  displayBoard();
+
+  return {
+    displayBoard,
+    placeMarker,
+    checkForWin
+  };
+
+})();
+
+const winningCombos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+
